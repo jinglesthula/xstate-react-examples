a47889066533b86f24b07bf7ebe051bade742ab0
Jon Anderson <jonathan.anderson@byu.edu>
2020-07-01 21:23:16 -0600

    TicTacToe mvp working now

--- tictactoe/src/components/App.js
+++ tictactoe/src/components/App.js
@@ -6,17 +6,19 @@ import { Board } from './Board'
 export const App = () => {
   const [state, send] = useMachine(TicTacToeMachine, { devTools: true })
   const { turn, board } = state.context
-  const screen = 'PLAYING'
 
   return (
     <MachineContext.Provider value={[state, send]}>
       <h1>Tic Tac Toe</h1>
       <Board board={board} />
-      {screen === 'PLAYING' && <span>Player {turn} turn</span>}
-      {screen === 'END' && (
-        <button type="button" onClick={() => console.log('reset!')}>
-          RESET
-        </button>
+      {state.matches('playing') && <span>Player {turn} turn</span>}
+      {state.matches('gameOver') && (
+        <>
+          <p>Player {state.context.turn === 2 ? 1 : 2} Wins!</p>
+          <button type="button" onClick={() => send('RESET')}>
+            RESET
+          </button>
+        </>
       )}
     </MachineContext.Provider>
   )
--- tictactoe/src/state/index.js
+++ tictactoe/src/state/index.js
@@ -3,45 +3,89 @@ import { Machine, assign } from 'xstate'
 
 export const MachineContext = React.createContext()
 
-export const TicTacToeMachine = Machine({
-  id: 'tictactoe',
-  initial: 'playing',
-  context: {
-    turn: 1,
-    board: [
-      [null, null, null],
-      [null, null, null],
-      [null, null, null],
-    ],
-  },
-  states: {
-    playing: {
-      on: {
-        TAKE_TURN: {
-          actions: assign({
-            turn: (context, event) => (context.turn === 1 ? 2 : 1),
-            board: (context, event) => {
-              const { board, turn } = context
-              return [
-                ...board.slice(0, event.y),
-                [
-                  ...board[event.y].slice(0, event.x),
-                  turn,
-                  ...board[event.y].slice(event.x + 1),
-                ],
-                ...board.slice(event.y + 1),
-              ]
-            },
-          }),
+const initialBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+]
+
+const column = (board) =>
+  [0, 1, 2].some(
+    (col) =>
+      board[0][col] &&
+      board[0][col] === board[1][col] &&
+      board[1][col] === board[2][col],
+  )
+
+const row = (board) =>
+  board.some((row, index) => row[0] && row[0] === row[1] && row[1] === row[2])
+
+const diagonal = (board) =>
+  board[1][1] &&
+  ((board[0][0] === board[1][1] && board[1][1] === board[2][2]) ||
+    (board[0][2] === board[1][1] && board[1][1] === board[2][0]))
+
+const cats = (board) => !board.some((row) => !row[0] || !row[1] || !row[2])
+
+export const TicTacToeMachine = Machine(
+  {
+    id: 'tictactoe',
+    initial: 'playing',
+    context: {
+      turn: 1,
+      board: initialBoard,
+    },
+    states: {
+      playing: {
+        on: {
+          '': {
+            target: 'gameOver',
+            cond: 'gameOver',
+          },
+          TAKE_TURN: {
+            target: 'playing',
+            actions: assign({
+              turn: (context, event) => (context.turn === 1 ? 2 : 1),
+              board: (context, event) => {
+                const { board, turn } = context
+                return [
+                  ...board.slice(0, event.y),
+                  [
+                    ...board[event.y].slice(0, event.x),
+                    turn,
+                    ...board[event.y].slice(event.x + 1),
+                  ],
+                  ...board.slice(event.y + 1),
+                ]
+              },
+            }),
+          },
+        },
+      },
+      gameOver: {
+        on: {
+          RESET: {
+            target: 'playing',
+            actions: assign({
+              board: () => initialBoard,
+              turn: () => 1,
+            }),
+          },
         },
       },
     },
-    end: {
-      on: {
-        RESET: {
-          // ?
-        },
+  },
+  {
+    guards: {
+      gameOver: (context, event) => {
+        const { board } = context
+        return (
+          row(board, event) ||
+          column(board, event) ||
+          diagonal(board, event) ||
+          cats(board, event)
+        )
       },
     },
   },
-})
+)
